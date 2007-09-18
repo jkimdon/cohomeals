898e62aa887825515bc92efff4ba8ea15a6f18aa
jkimdon <jkimdon@f0a5714d-0346-0410-9620-992f67b9492e>
2007-09-18 15:10:17 +0000

    fixed meal deletion -- does delete now. no checks on whether meal has occurred, nor have I tested email notification to signed up users

--- cohomeals/trunk/del_entry.php
+++ cohomeals/trunk/del_entry.php
@@ -6,32 +6,13 @@ $can_edit = false;
 // First, check to see if this user should be able to delete this event.
 if ( $id > 0 ) {
   // first see who has access to edit this entry
-  if ( $is_admin ) {
+  if ( $is_admin || $is_meal_coordinator ) {
     $can_edit = true;
-  } else if ( $readonly == "Y" ) {
-    $can_edit = false;
-  } else {
-    $can_edit = false;
-    $sql = "SELECT webcal_meal.cal_id FROM webcal_meal, " .
-      "webcal_meal_participant WHERE webcal_meal.cal_id = " .
-      "webcal_meal_participant.cal_id AND webcal_meal.cal_id = $id " .
-      "AND webcal_meal_participant.cal_login = '$login'";
-    $res = dbi_query ( $sql );
-    if ( $res ) {
-      $row = dbi_fetch_row ( $res );
-      if ( $row && $row[0] > 0 )
-        $can_edit = true;
-      dbi_free_result ( $res );
-    }
   }
 }
 
-
-if ( $readonly == 'Y' )
-  $can_edit = false;
-
 if ( ! $can_edit ) {
-  $error = translate ( "You are not authorized" );
+  $error = "You are not authorized";
 }
 
 if ( $id > 0 && empty ( $error ) ) {
@@ -46,88 +27,60 @@ if ( $id > 0 && empty ( $error ) ) {
     }
   }
 
-  // Only allow delete of webcal_meal
-  // if admin, not participant.
-  if ( $is_admin ) {
-
-    // Email participants that the event was deleted
-    // First, get list of participants 
-    $sql = "SELECT cal_login FROM webcal_meal_participant WHERE cal_id = $id ";
-    $res = dbi_query ( $sql );
-    $partlogin = array ();
-    if ( $res ) {
-      while ( $row = dbi_fetch_row ( $res ) ) {
-        if ( $row[0] != $login )
-   $partlogin[] = $row[0];
-      }
-      dbi_free_result($res);
+  // Email participants that the event was deleted
+  // First, get list of participants 
+  $sql = "SELECT cal_login FROM webcal_meal_participant WHERE cal_id = $id ";
+  $res = dbi_query ( $sql );
+  $partlogin = array ();
+  if ( $res ) {
+    while ( $row = dbi_fetch_row ( $res ) ) {
+      if ( $row[0] != $login )
+	$partlogin[] = $row[0];
     }
-
-    // Get event name
-    $sql = "SELECT cal_suit, cal_date, cal_time " .
-      "FROM webcal_meal WHERE cal_id = $id";
-    $res = dbi_query($sql);
-    if ( $res ) {
-      $row = dbi_fetch_row ( $res );
-      $name = $row[0];
-      $eventdate = $row[1];
-      $eventtime = $row[2];
-      dbi_free_result ( $res );
-    }
-    $TIME_FORMAT=24;
-    for ( $i = 0; $i < count ( $partlogin ); $i++ ) {
-      // Log the deletion
-      activity_log ( $id, $login, $partlogin[$i], $LOG_DELETE, "" );
-
-      $do_send = get_pref_setting ( $partlogin[$i], "EMAIL_EVENT_DELETED" );
-      $user_TZ = get_pref_setting ( $partlogin[$i], "TZ_OFFSET" );
-      $user_language = get_pref_setting ( $partlogin[$i], "LANGUAGE" );
-      user_load_variables ( $partlogin[$i], "temp" );
-      // Want date/time in user's timezone
-      if ( $eventtime != '-1' ) { 
-        $eventtime += ( $user_TZ * 10000 );
-        if ( $eventtime < 0 ) {
-          $eventtime += 240000;
-        } else if ( $eventtime >= 240000 ) {
-          $eventtime -= 240000;
-        }
-      }            
-      if ( $partlogin[$i] != $login && $do_send == "Y" && 
-        strlen ( $tempemail ) && $send_email != "N" ) {
-         if (($GLOBALS['LANGUAGE'] != $user_language) && ! empty ( $user_language ) && ( $user_language != 'none' )){
-          reset_language ( $user_language );
-        }
-        $msg = translate("Hello") . ", " . $tempfullname . ".\n\n" .
-          translate("An appointment has been canceled for you by") .
-          " " . $login_fullname .  ".\n" .
-          translate("The subject was") . " \"" . $name . "\"\n" .
-          translate("Date") . ": " . date_to_str ($thisdate) . "\n";
-          if ( $eventtime != '-1' ) $msg .= translate("Time") . ": " . display_time ($eventtime, true);
-          $msg .= "\n\n";
-        if ( strlen ( $login_email ) )
-          $extra_hdrs = "From: $login_email\r\nX-Mailer: " .
-            translate($application_name);
-        else
-          $extra_hdrs = "From: $email_fallback_from\r\nX-Mailer: " .
-            translate($application_name);
-        mail ( $tempemail,
-          translate($application_name) . " " .
-   translate("Notification") . ": " . $name,
-          html_to_8bits ($msg), $extra_hdrs );
-      }
-    }
-
-    // Now, mark event as deleted for all users.
-    dbi_query ( "DELETE FROM webcal_meal_participant " .
-      "WHERE cal_id = $id" );
-
-  } else {
-    // Not the admin.
-    // Just delete the event from this user's calendar.
-    dbi_query ( "DELETE FROM webcal_meal_participant " .
-      "WHERE cal_id = $id AND cal_login = '$login'" );
-    activity_log ( $id, $login, $login, $LOG_REJECT, "" );
+    dbi_free_result($res);
   }
+
+  // Get event name
+  $sql = "SELECT cal_suit, cal_date, cal_time " .
+    "FROM webcal_meal WHERE cal_id = $id";
+  $res = dbi_query($sql);
+  if ( $res ) {
+    $row = dbi_fetch_row ( $res );
+    $name = $row[0];
+    $eventdate = $row[1];
+    $eventtime = $row[2];
+    dbi_free_result ( $res );
+  }
+  for ( $i = 0; $i < count ( $partlogin ); $i++ ) {
+    // Log the deletion
+    activity_log ( $id, $login, $partlogin[$i], $LOG_DELETE, "" );
+
+    $do_send = get_pref_setting ( $partlogin[$i], "EMAIL_EVENT_DELETED" );
+    user_load_variables ( $partlogin[$i], "temp" );
+    if ( $partlogin[$i] != $login && $do_send == "Y" && 
+	 strlen ( $tempemail ) && $send_email != "N" ) {
+      $msg = "Hello " . $tempfullname . ".\n\n" .
+	"A meal you were signed up for has been canceled by " .
+	$login_fullname .  ".\n" .
+	"The suit was " . $name . "\n" .
+	"Date: " . date_to_str ($thisdate) . "\n";
+      if ( $eventtime != '-1' ) $msg .= "Time: " . display_time ($eventtime, true);
+      $msg .= "\n\n";
+      if ( strlen ( $login_email ) )
+	$extra_hdrs = "From: $login_email\r\nX-Mailer: " . $application_name;
+      else
+	$extra_hdrs = "From: $email_fallback_from\r\nX-Mailer: " . $application_name;
+      mail ( $tempemail,
+	     $application_name . " " .
+	     "Notification: " . $name,
+	     html_to_8bits ($msg), $extra_hdrs );
+    }
+  }
+
+  // Now, mark event as deleted for all users.
+  dbi_query ( "DELETE FROM webcal_meal_participant WHERE cal_id = $id" );
+  dbi_query ( "DELETE FROM webcal_meal WHERE cal_id = $id" );
+  
 }
 
 $ret = getValue ( "ret" );
