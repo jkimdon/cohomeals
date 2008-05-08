93bde6610cac34a44c39c6ab4d453215d9d4ceb5
jkimdon <jkimdon@f0a5714d-0346-0410-9620-992f67b9492e>
2008-05-08 22:22:00 +0000

    removed subscriptions since they're not working right now

--- cohomeals/trunk/includes/trailer.php
+++ cohomeals/trunk/includes/trailer.php
@@ -58,9 +58,9 @@ if ( $login != '__public__' && $readonly == 'N' ) {
 
 
 /// adding/subscribing links
-$subscribe_link[] = "<a href=\"subscribe_diamondBROKEN.php\">Diamond</a>";
-$subscribe_link[] = "<a href=\"subscribe_heartBROKEN.php\">Heart</a>";
-$subscribe_link[] = "<a href=\"subscribe_club.php\">Club</a>";
+//$subscribe_link[] = "<a href=\"subscribe_diamondBROKEN.php\">Diamond</a>";
+//$subscribe_link[] = "<a href=\"subscribe_heartBROKEN.php\">Heart</a>";
+//$subscribe_link[] = "<a href=\"subscribe_club.php\">Club</a>";
 $url = "<a title=\"Add New Meal\" href=\"edit_entry.php";
 if ( ! empty ( $thisyear ) ) {
   $url .= "?year=$thisyear";
