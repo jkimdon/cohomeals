0cbdfdf391b8960f012311fded701a3a375eb975
jkimdon <jkimdon@f0a5714d-0346-0410-9620-992f67b9492e>
2008-12-09 02:18:28 +0000

    changed mirror scripts to work with google code

--- cohomeals/trunk/tools/mirror.sh
+++ cohomeals/trunk/tools/mirror.sh
@@ -6,7 +6,8 @@ set -x
 
 cd ~/
 rm -rf coho-mirror
-svn export file:///var/svn/coho/cohomeals/trunk coho-mirror
+svn export http://cohomeals.googlecode.com/svn/cohomeals/trunk coho-mirror
+#svn export file:///var/svn/coho/cohomeals/trunk coho-mirror
 cd coho-mirror
 for f in `cat $TOOLS/mirror.exclude`; do
   rm -rf $f
@@ -15,8 +16,9 @@ done
 # include the reminder script, even though it is contained within the tools/
 # directory, excluded in mirror.exclude above.
 mkdir -p tools
-svn export file:///var/svn/coho/cohomeals/trunk/tools/weekly_reminder.php \
-       tools/weekly_reminder.php
+svn export http://cohomeals.googlecode.com/svn/cohomeals/trunk/tools/weekly_reminder.php tools/weekly_reminder.php
+#svn export file:///var/svn/coho/cohomeals/trunk/tools/weekly_reminder.php \
+#       tools/weekly_reminder.php
 
 lftp -f $TOOLS/mirror.lftp
 
